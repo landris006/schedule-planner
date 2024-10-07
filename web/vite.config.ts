f2dbd5df6622ef3d795fa42d861dbdd89b60745b
landris006 <laczko.andris007@gmail.com>
2024-10-07 19:39:41 +0200

    path alias

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,9 +8,14 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      "/api": {
-        target: "http://localhost:8080/",
+      '/api': {
+        target: 'http://localhost:8080/',
       },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
