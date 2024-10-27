0d7528de32807fc35af36da254ddc4d48ad4f520
landris006 <laczko.andris007@gmail.com>
2024-10-27 16:52:01 +0100

    eslint unused error fix

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -28,6 +28,7 @@ export default tseslint.config(
         {
           argsIgnorePattern: '^_',
           varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
         },
       ],
     },
