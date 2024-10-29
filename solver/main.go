88b779ef2c6d639fcc503c305047dd3fbdc5ffbf
Dávid Závogyán <zavogyandavid@gmail.com>
2024-10-29 19:03:29 +0100

    solver type clarification, enum workaround :(

--- solver/main.go
+++ solver/main.go
@@ -6,41 +6,60 @@ package main
 // preferenciák:
 // lyukas óra minimalizálás
 
+//Tantárgy
+type Subject struct {
+	Code    string    `json:"code"`
+	Name    string    `json:"name"`
+	Courses []*Course `json:"courses"`
+}
+
+//Kurzus
 type Course struct {
-	Code    string   `json:"code"`
-	Name    string   `json:"name"`
-	Classes []*Class `json:"classes"`
+	Subject    *Subject   `json:"subject"`
+	Code       string     `json:"code"`
+	Name       string     `json:"name"`
+	Time       Time       `json:"time"`
+	Instructor string     `json:"instructor"`
+	Location   string     `json:"location"`
+	Capacity   int        `json:"capacity"`
+	Type       CourseType `json:"type"` // 0 = 'lecture' | 1 = 'practice'
 }
 
-type Class struct {
-	Time       Time   `json:"time"`
-	Code       string `json:"code"`
-	Instructor string `json:"instructor"`
-	Place      string `json:"place"`
-	Capacity   int    `json:"capacity"`
-	Type       int    `json:"type"` // 0 = 'lecture' | 1 = 'practice'
-}
-
+//5 egész percekre kerekített diszkrét értékek
 type Time struct {
-	Start int `json:"start"` // 0 - 23.99
-	End   int `json:"end"`   // 0 - 23.99
-	Day   int `json:"day"`   // 0 = Monday, 1 = Tuesday, 2 = Wednesday, 3 = Thursday, 4 = Friday, 5 = Saturday, 6 = Sunday
+	Start int     `json:"start"` // 0 - 287
+	End   int     `json:"end"`   // 0 - 287
+	Day   Weekday `json:"day"`   // 0 = Monday, 1 = Tuesday, 2 = Wednesday, 3 = Thursday, 4 = Friday, 5 = Saturday, 6 = Sunday
 }
 
-const (
-	monday    = iota
-	tuesday   = iota
-	wednesday = iota
-	thursday  = iota
-	friday    = iota
-	saturday  = iota
-	sunday    = iota
-)
+//A Golang nem támogatja az Enum-ot, workaround:
+//https://builtin.com/software-engineering-perspectives/golang-enum
+type Weekday int
 
 const (
-	lecture  = iota
-	practice = iota
+	Monday Weekday = iota
+	Tuesday
+	Wednesday
+	Thursday
+	Friday
+	Saturday
+	Sunday
 )
+
+func (w Weekday) ordinal() int {
+	return int(w)
+}
+
+type CourseType int
+
+const (
+	Lecture CourseType = iota
+	Practice
+)
+
+func (ct CourseType) ordinal() int {
+	return int(ct)
+}
 
 func main() {
 	println("Hello, world!")
