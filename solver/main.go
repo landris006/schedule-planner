73d412ed8d773bcaa75ebb4c58a96b433591fd95
Dávid Závogyán <zavogyandavid@gmail.com>
2024-11-01 15:30:24 +0100

    A tantárgyakkal kapcsolatos adatok külön fájlba, beolvasás json-ból, minta json

--- solver/main.go
+++ solver/main.go
@@ -1,66 +1,32 @@
 package main
 
+import "strconv"
+
 // hard feltételek:
 // ne legyen óra ütközés
 
 // preferenciák:
 // lyukas óra minimalizálás
 
-//Tantárgy
-type Subject struct {
-	Code    string    `json:"code"`
-	Name    string    `json:"name"`
-	Courses []*Course `json:"courses"`
-}
-
-//Kurzus
-type Course struct {
-	Subject    *Subject   `json:"subject"`
-	Code       string     `json:"code"`
-	Name       string     `json:"name"`
-	Time       Time       `json:"time"`
-	Instructor string     `json:"instructor"`
-	Location   string     `json:"location"`
-	Capacity   int        `json:"capacity"`
-	Type       CourseType `json:"type"` // 0 = 'lecture' | 1 = 'practice'
-}
-
-//5 egész percekre kerekített diszkrét értékek
-type Time struct {
-	Start int     `json:"start"` // 0 - 287
-	End   int     `json:"end"`   // 0 - 287
-	Day   Weekday `json:"day"`   // 0 = Monday, 1 = Tuesday, 2 = Wednesday, 3 = Thursday, 4 = Friday, 5 = Saturday, 6 = Sunday
-}
-
-//A Golang nem támogatja az Enum-ot, workaround:
-//https://builtin.com/software-engineering-perspectives/golang-enum
-type Weekday int
-
-const (
-	Monday Weekday = iota
-	Tuesday
-	Wednesday
-	Thursday
-	Friday
-	Saturday
-	Sunday
-)
-
-func (w Weekday) ordinal() int {
-	return int(w)
-}
-
-type CourseType int
-
-const (
-	Lecture CourseType = iota
-	Practice
-)
-
-func (ct CourseType) ordinal() int {
-	return int(ct)
-}
-
 func main() {
-	println("Hello, world!")
+	var subjects = ReadSubjects("test_inputs/test_a.json")
+
+	for i := 0; i < len(subjects); i++ {
+		println("")
+		println("Subject name: " + subjects[i].Name)
+		println("Subject code: " + subjects[i].Code)
+		println("Subject Courses:")
+		for j := 0; j < len(subjects[i].Courses); j++ {
+			println("\tCourse code: " + subjects[i].Courses[j].Code)
+			println("\tCourse type: " + strconv.Itoa(subjects[i].Courses[j].Type.ordinal()))
+			println("\tCourse Day: " + strconv.Itoa(subjects[i].Courses[j].Time.Day.ordinal()))
+			println("\tCourse Start: " + strconv.FormatFloat(float64(subjects[i].Courses[j].Time.Start), 'g', 2, 32))
+			println("\tCourse End: " + strconv.FormatFloat(float64(subjects[i].Courses[j].Time.End), 'g', 2, 32))
+			println("\tCourse Location: " + subjects[i].Courses[j].Location)
+			println("\tCourse Instructor: " + subjects[i].Courses[j].Instructor)
+			println("")
+		}
+		println("")
+	}
+
 }
