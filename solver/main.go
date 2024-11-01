a7111b1dcc1da86dcddca4349b41f0b1a1a3eeb3
Gergo Turan <gerituran@gmail.com>
2024-11-01 10:34:00 +0100

    funtion for course compatibility

--- solver/main.go
+++ solver/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // hard feltételek:
 // ne legyen óra ütközés
 
@@ -61,6 +63,41 @@ func (ct CourseType) ordinal() int {
 	return int(ct)
 }
 
+func courseCompatible(course1 Course, course2 Course) bool {
+	if course1.Time.Day != course2.Time.Day { // courses are on different days
+		return true
+	} else if course1.Time.End < course2.Time.Start { // first course ends before second course starts
+		return true
+	} else if course2.Time.End < course1.Time.Start { // second course ends before first course starts
+		return true
+	} else { // courses intersect
+		return false
+	}
+}
+
 func main() {
-	println("Hello, world!")
+	var sub = Subject{"", "", []*Course{}}
+	var course1 = Course{&sub, "", "", Time{8.0, 8.50, Wednesday}, "", "", 100, Lecture}
+	var course2 = Course{&sub, "", "", Time{8.0, 8.50, Wednesday}, "", "", 100, Lecture}
+	fmt.Println(courseCompatible(course1, course2)) // false
+
+	course1.Time.Day = Tuesday
+	fmt.Println(courseCompatible(course1, course2)) // true
+
+	course2.Time.Day = Tuesday
+	course2.Time.Start = 9.0
+	course2.Time.End = 9.50
+	fmt.Println(courseCompatible(course1, course2)) // true
+
+	course1.Time.Start = 9.0
+	course1.Time.End = 9.50
+	course2.Time.Start = 8.0
+	course2.Time.End = 8.50
+	fmt.Println(courseCompatible(course1, course2)) // true
+
+	course1.Time.Start = 7.0
+	course1.Time.End = 9.50
+	course2.Time.Start = 8.0
+	course2.Time.End = 8.50
+	fmt.Println(courseCompatible(course1, course2)) // false
 }
