387efb44c7ce62905590228c486b712b38821137
landris006 <laczko.andris007@gmail.com>
2024-12-21 17:02:13 +0100

    removed unnecessary import

--- web/src/utils.ts
+++ web/src/utils.ts
@@ -1,7 +1,6 @@
 import { clsx, type ClassValue } from 'clsx';
 import { useState } from 'react';
 import { twMerge } from 'tailwind-merge';
-import { Subject } from './contexts/subjects/subjects-context';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
